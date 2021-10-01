8065f814af0aeee2c9846d32fef0dc8b704f37c2
lux <luischn94@gmail.com>
2021-10-01 10:51:22 -0300

    updating

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,3 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
-
-
-
-# "/article/1"
-# "/articles" => "/"
-
-# localhost:3000/articles
-# localhost:3000/
